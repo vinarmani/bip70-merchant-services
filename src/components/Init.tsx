36aecf81d7ab452ef6f32fdf87cb7185daddd4e7
paool <paoolkim@gmail.com>
2019-08-22 15:46:04 -0700

    style init page

--- src/components/Init.tsx
+++ src/components/Init.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 
 import { Link } from "react-router-dom";
+import cryptoSVG from "../../src/img/crypto.svg"
 
 
 export interface InitProps {
@@ -35,11 +36,24 @@ export class Init extends React.Component<InitProps, any> {
   render(): JSX.Element {
     const { isValid } = this.state
     return (
-      <div>
-        <h1>enter your merchant id</h1>
-        <input type="text" onChange={e => { this.handleOnChange(e) }} />
-        {isValid && <Link to="/invoice"> Proceed </Link>}
+      <div className="wrapper">
+        <h1 className="merchant-header">Welcome to your Bitcoin Cash Register</h1>
+        <img src={cryptoSVG}  style={{maxHeight: '30vh'}}/>
+        <div className="merchant-input">
+          <label htmlFor="merchant">Enter merchant ID</label>
+          <input
+          name="merchant"
+          type="text"
+          onChange={e => {
+            this.handleOnChange(e);
+            }}
+          />
+        </div>
+
+        <div className={`merchant-proceed ${isValid ? 'active' : 'disabled'}`}>
+          <Link to="/invoice"> Proceed </Link>
+        </div>
       </div>
-    )
+    );
   }
 }
